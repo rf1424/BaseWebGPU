63d608de7f067b9420e38b9cdb0beb804ff660b3
RFK <kakigori11235@gmail.com>
2025-09-02 20:20:43 -0400

    Refactoring and Code organization (Application/Dependencies/Main)

---
+++ Application.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+#include "webgpu/webgpu.hpp" 
+#include "VertexAttr.h"
+#include "Camera.h"
+
+#include <GLFW/glfw3.h>
+#include <glm/ext.hpp>
+#include <vector>
+#include <filesystem>
+
+using namespace wgpu;
+
+class Application {
+public:
+    bool Initialize();
+    void Terminate();
+    void MainLoop();
+    bool IsRunning();
+
+private:
+    GLFWwindow* window;
+    Device device;
+    Adapter adapter;
+    Queue queue;
+    Surface surface;
+    std::unique_ptr<ErrorCallback> uncapturedErrorCallbackHandle; // TODO
+    RenderPipeline pipeline;
+    TextureFormat surfaceFormat = TextureFormat::Undefined;
+
+    // uniform bindings
+    BindGroupLayout bindGroupLayout;
+    BindGroup bindGroup;
+    PipelineLayout layout;
+
+    // buffers
+    Buffer vertexBuffer;
+    // Buffer indexBuffer;
+    Buffer uniformBuffer;
+
+    struct Uniforms {
+        glm::mat4x4 projMatrix; // alignment: 16
+        glm::mat4x4 viewMatrix;
+        glm::mat4x4 modelMatrix;
+        float time;
+        // padding
+        float padding[3]; // time + pad = 16 bytes for alignment!
+    };
+
+    uint32_t indexCount = 0;
+
+    //depth setup
+    Texture depthTexture;
+    TextureView depthTextureView;
+    TextureFormat depthTextureFormat = TextureFormat::Undefined;
+
+    TextureView colorTextureView; // TODO: textureformat?
+    Sampler sampler;
+
+    Camera viewCamera;
+
+
+private:
+    TextureView GetNextSurfaceTextureView();
+    void InitializePipeline();
+    RequiredLimits GetRequiredLimits(Adapter adapter) const;
+    void InitializeSurface();
+    void InitializeBuffers();
+    void InitializeBindGroups();
+    void InitializeDepthTexture();
+    Texture getObjTexture(const std::filesystem::path& path, Device device, TextureView* textureView = nullptr);
+
+    void reSizeScreen();
+    // camera methods
+    void updateViewMatrix();
+    void onClick(int button, int action, int);
+    void onDrag(double xpos, double ypos);
+    void onScroll(double xoffset, double yoffset);
+};
